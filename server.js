5b6ee8882cd228635b3e79c8f624ffdc9ac60022
Kevin <stiivenmendez16@gmail.com>
2023-12-07 13:04:34 -0500

    CRUD

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import { createServer } from 'http'
+import usersRouter from './src/routes/userRoutes.js'
 
 const app = express()
-
+app.use(cors())
+app.use(express.json())
+app.use('/api', usersRouter)
 dotenv.config()
 const server = createServer(app)
 
